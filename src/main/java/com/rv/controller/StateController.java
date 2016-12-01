92bdc98d2bdc963df21195c5db5f48acc49609b8
Nikesh Maharjan <nickesh.maharjan@gmail.com>
2016-12-01 16:50:39 -0600

    more Clean Code

--- src/main/java/com/rv/controller/StateController.java
+++ src/main/java/com/rv/controller/StateController.java
@@ -1,8 +1,7 @@
 package com.rv.controller;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
